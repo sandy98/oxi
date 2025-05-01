2f1f189caacf68e482f951d6e3074426f190364d
sandy98 <esavoretti@gmail.com>
2025-05-01 03:55:31 -0300

    Finishing file sending service

--- build/lib/oxi/__init__.py
+++ build/lib/oxi/__init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-__version__ = "0.1.7"
+__version__ = "0.2.1"
 __author__ = "Domingo E. Savoretti"
 __email__ = "esavoretti@gmail.com"
 __license__ = "MIT"
 __url__ = "https://github.com/sandy98/oxi"
-__description__ = "A simple library to read and write dbase III files."
+__description__ = "ASGI full stack: Web Server, Web App and a ton of goodies."
 __copyright__ = "Copyright (c) 2025 Domingo E. Savoretti"
 __oxi_port__ = 8086
 __oxi_host__ = "0.0.0.0"
